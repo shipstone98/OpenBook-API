2eaa933b078cc24d72b1fd266931d731c39b4c86
Christopher Shipstone <christopher.shipstone98@gmail.com>
2025-09-04 11:38:36 +0100

    Fixed response

--- src/web/Models/Post/RetrieveResponse.cs
+++ src/web/Models/Post/RetrieveResponse.cs
@@ -8,12 +8,12 @@ internal sealed class RetrieveResponse
 {
     private readonly IPost _post;
 
-    String Body => this._post.Body;
-    DateTime Created => this._post.Created;
-    String Creator => this._post.CreatorName;
-    long Id => this._post.Id;
-    Nullable<long> ParentId => this._post.ParentId;
-    DateTime Updated => this._post.Updated;
+    public String Body => this._post.Body;
+    public DateTime Created => this._post.Created;
+    public String Creator => this._post.CreatorName;
+    public long Id => this._post.Id;
+    public Nullable<long> ParentId => this._post.ParentId;
+    public DateTime Updated => this._post.Updated;
 
     internal RetrieveResponse(IPost post) => this._post = post;
 }
