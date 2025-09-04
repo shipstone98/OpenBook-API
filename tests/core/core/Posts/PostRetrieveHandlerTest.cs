bbc1003943322c8fa9ff443dfd544f7fcbc2eb26
Christopher Shipstone <christopher.shipstone98@gmail.com>
2025-09-04 11:38:56 +0100

    Added missing test

--- tests/core/core/Posts/PostRetrieveHandlerTest.cs
+++ tests/core/core/Posts/PostRetrieveHandlerTest.cs
@@ -61,7 +61,32 @@ public sealed class PostRetrieveHandlerTest
 
 #region Valid arguments
     [Fact]
-    public async Task TestHandleAsync_Valid_Failure()
+    public async Task TestHandleAsync_Valid_Failure_CreatorNotFound()
+    {
+        // Arrange
+        this._repository._postsFunc = () =>
+        {
+            MockPostRepository posts = new();
+            posts._retrieveFunc = _ => new();
+            return posts;
+        };
+
+        this._claims._isAuthenticatedFunc = () => false;
+
+        this._repository._usersFunc = () =>
+        {
+            MockUserRepository users = new();
+            users._retrieve_GuidFunc = _ => null;
+            return users;
+        };
+
+        // Act and assert
+        await Assert.ThrowsAsync<NotFoundException>(() =>
+            this._handler.HandleAsync(12345, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task TestHandleAsync_Valid_Failure_PostNotFound()
     {
         // Arrange
         this._repository._postsFunc = () =>
@@ -79,71 +104,6 @@ public sealed class PostRetrieveHandlerTest
 #region Success
     [Fact]
     public async Task TestHandleAsync_Valid_Success_Authenticated_Creator()
-    {
-#region Arrange
-        // Arrange
-        const long ID = 12345;
-        DateTime created = DateTime.UnixEpoch.ToUniversalTime();
-        DateTime updated = created.AddDays(12345);
-        const String CREATOR_EMAIL_ADDRESS = "john.doe@contoso.com";
-        const String CREATOR_USER_NAME = "johndoe2025";
-        const long PARENT_ID = 67890;
-        const String BODY = "Hello, world!";
-
-        this._repository._postsFunc = () =>
-        {
-            MockPostRepository posts = new();
-
-            posts._retrieveFunc = id =>
-                new PostEntity
-                {
-                    Body = BODY,
-                    Created = created,
-                    CreatorId = Guid.NewGuid(),
-                    Id = ID,
-                    ParentId = PARENT_ID,
-                    Updated = updated
-                };
-
-            return posts;
-        };
-
-        this._claims._isAuthenticatedFunc = () => true;
-        this._claims._idFunc = Guid.NewGuid;
-
-        this._repository._usersFunc = () =>
-        {
-            MockUserRepository users = new();
-
-            users._retrieve_GuidFunc = _ =>
-                new UserEntity
-                {
-                    EmailAddress = CREATOR_EMAIL_ADDRESS,
-                    UserName = CREATOR_USER_NAME
-                };
-
-            return users;
-        };
-#endregion
-
-        // Act
-        IPost post =
-            await this._handler.HandleAsync(ID, CancellationToken.None);
-
-        // Assert
-        post.AssertEqual(
-            ID,
-            created,
-            updated,
-            CREATOR_EMAIL_ADDRESS,
-            CREATOR_USER_NAME,
-            BODY,
-            PARENT_ID
-        );
-    }
-
-    [Fact]
-    public async Task TestHandleAsync_Valid_Success_Authenticated_NotCreator()
     {
 #region Arrange
         // Arrange
@@ -178,6 +138,70 @@ public sealed class PostRetrieveHandlerTest
         this._claims._idFunc = () => creatorId;
         this._claims._emailAddressFunc = () => CREATOR_EMAIL_ADDRESS;
         this._claims._userNameFunc = () => CREATOR_USER_NAME;
+#endregion
+
+        // Act
+        IPost post =
+            await this._handler.HandleAsync(ID, CancellationToken.None);
+
+        // Assert
+        post.AssertEqual(
+            ID,
+            created,
+            updated,
+            CREATOR_EMAIL_ADDRESS,
+            CREATOR_USER_NAME,
+            BODY,
+            PARENT_ID
+        );
+    }
+
+    [Fact]
+    public async Task TestHandleAsync_Valid_Success_Authenticated_NotCreator()
+    {
+#region Arrange
+        // Arrange
+        const long ID = 12345;
+        DateTime created = DateTime.UnixEpoch.ToUniversalTime();
+        DateTime updated = created.AddDays(12345);
+        const String CREATOR_EMAIL_ADDRESS = "john.doe@contoso.com";
+        const String CREATOR_USER_NAME = "johndoe2025";
+        const long PARENT_ID = 67890;
+        const String BODY = "Hello, world!";
+
+        this._repository._postsFunc = () =>
+        {
+            MockPostRepository posts = new();
+
+            posts._retrieveFunc = id =>
+                new PostEntity
+                {
+                    Body = BODY,
+                    Created = created,
+                    Id = ID,
+                    ParentId = PARENT_ID,
+                    Updated = updated
+                };
+
+            return posts;
+        };
+
+        this._claims._isAuthenticatedFunc = () => true;
+        this._claims._idFunc = Guid.NewGuid;
+
+        this._repository._usersFunc = () =>
+        {
+            MockUserRepository users = new();
+
+            users._retrieve_GuidFunc = _ =>
+                new UserEntity
+                {
+                    EmailAddress = CREATOR_EMAIL_ADDRESS,
+                    UserName = CREATOR_USER_NAME
+                };
+
+            return users;
+        };
 #endregion
 
         // Act
