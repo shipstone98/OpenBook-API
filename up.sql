f370aedf54fdd1c21292bccd704179749c1d3f9a
Christopher Shipstone <christopher.shipstone98@gmail.com>
2026-04-14 11:19:26 +0100

    Added UP script

---
+++ up.sql
@@ -0,0 +1,533 @@
+﻿CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
+    `MigrationId` varchar(150) CHARACTER SET utf8mb4 NOT NULL,
+    `ProductVersion` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
+    CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
+) CHARACTER SET=utf8mb4;
+
+START TRANSACTION;
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250826123152_InitialCreate') THEN
+
+    ALTER DATABASE CHARACTER SET utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250826123152_InitialCreate') THEN
+
+    CREATE TABLE `Users` (
+        `Id` char(36) COLLATE ascii_general_ci NOT NULL,
+        `Consented` datetime(6) NOT NULL,
+        `EmailAddress` longtext CHARACTER SET utf8mb4 NOT NULL,
+        `EmailAddressNormalized` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+        `Forename` longtext CHARACTER SET utf8mb4 NOT NULL,
+        `IsActive` tinyint(1) NOT NULL,
+        `Otp` varchar(6) CHARACTER SET utf8mb4 NULL,
+        `OtpExpires` datetime(6) NULL,
+        `PasswordHash` longtext CHARACTER SET utf8mb4 NULL,
+        `Surname` longtext CHARACTER SET utf8mb4 NOT NULL,
+        `UserName` longtext CHARACTER SET utf8mb4 NOT NULL,
+        `UserNameNormalized` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+        `Created` datetime(6) NOT NULL,
+        `Updated` datetime(6) NOT NULL,
+        CONSTRAINT `PK_Users` PRIMARY KEY (`Id`)
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250826123152_InitialCreate') THEN
+
+    CREATE UNIQUE INDEX `IX_Users_EmailAddressNormalized` ON `Users` (`EmailAddressNormalized`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250826123152_InitialCreate') THEN
+
+    CREATE UNIQUE INDEX `IX_Users_UserNameNormalized` ON `Users` (`UserNameNormalized`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250826123152_InitialCreate') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20250826123152_InitialCreate', '9.0.14');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250828110923_UserRoleRefreshToken') THEN
+
+    CREATE TABLE `Roles` (
+        `Id` bigint NOT NULL AUTO_INCREMENT,
+        `Name` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
+        `NameNormalized` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
+        `Created` datetime(6) NOT NULL,
+        `Updated` datetime(6) NOT NULL,
+        CONSTRAINT `PK_Roles` PRIMARY KEY (`Id`)
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250828110923_UserRoleRefreshToken') THEN
+
+    CREATE TABLE `UserRefreshTokens` (
+        `Id` bigint NOT NULL AUTO_INCREMENT,
+        `Expires` datetime(6) NOT NULL,
+        `UserId` char(36) COLLATE ascii_general_ci NOT NULL,
+        `Value` longtext CHARACTER SET utf8mb4 NOT NULL,
+        `Created` datetime(6) NOT NULL,
+        `Updated` datetime(6) NOT NULL,
+        CONSTRAINT `PK_UserRefreshTokens` PRIMARY KEY (`Id`),
+        CONSTRAINT `FK_UserRefreshTokens_Users_UserId` FOREIGN KEY (`UserId`) REFERENCES `Users` (`Id`) ON DELETE CASCADE
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250828110923_UserRoleRefreshToken') THEN
+
+    CREATE TABLE `UserRoles` (
+        `RoleId` bigint NOT NULL,
+        `UserId` char(36) COLLATE ascii_general_ci NOT NULL,
+        `Assigned` datetime(6) NOT NULL,
+        CONSTRAINT `PK_UserRoles` PRIMARY KEY (`UserId`, `RoleId`),
+        CONSTRAINT `FK_UserRoles_Roles_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `Roles` (`Id`) ON DELETE CASCADE,
+        CONSTRAINT `FK_UserRoles_Users_UserId` FOREIGN KEY (`UserId`) REFERENCES `Users` (`Id`) ON DELETE CASCADE
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250828110923_UserRoleRefreshToken') THEN
+
+    CREATE UNIQUE INDEX `IX_Roles_NameNormalized` ON `Roles` (`NameNormalized`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250828110923_UserRoleRefreshToken') THEN
+
+    CREATE INDEX `IX_UserRefreshTokens_UserId` ON `UserRefreshTokens` (`UserId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250828110923_UserRoleRefreshToken') THEN
+
+    CREATE INDEX `IX_UserRoles_RoleId` ON `UserRoles` (`RoleId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250828110923_UserRoleRefreshToken') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20250828110923_UserRoleRefreshToken', '9.0.14');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250828110934_RoleSeed') THEN
+
+    INSERT INTO `Roles` (`Id`, `Created`, `Updated`, `Name`, `NameNormalized`)
+    VALUES (2, TIMESTAMP '2026-04-14 10:19:12', TIMESTAMP '2026-04-14 10:19:12', 'Administrator', 'ADMINISTRATOR');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250828110934_RoleSeed') THEN
+
+    INSERT INTO `Roles` (`Id`, `Created`, `Updated`, `Name`, `NameNormalized`)
+    VALUES (3, TIMESTAMP '2026-04-14 10:19:12', TIMESTAMP '2026-04-14 10:19:12', 'System Administrator', 'SYSTEM ADMINISTRATOR');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250828110934_RoleSeed') THEN
+
+    INSERT INTO `Roles` (`Id`, `Created`, `Updated`, `Name`, `NameNormalized`)
+    VALUES (1, TIMESTAMP '2026-04-14 10:19:12', TIMESTAMP '2026-04-14 10:19:12', 'User', 'USER');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250828110934_RoleSeed') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20250828110934_RoleSeed', '9.0.14');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250901093419_Post') THEN
+
+    CREATE TABLE `Posts` (
+        `Id` bigint NOT NULL AUTO_INCREMENT,
+        `Body` varchar(512) CHARACTER SET utf8mb4 NOT NULL,
+        `ParentId` bigint NULL,
+        `Created` datetime(6) NOT NULL,
+        `Updated` datetime(6) NOT NULL,
+        `CreatorId` char(36) COLLATE ascii_general_ci NOT NULL,
+        CONSTRAINT `PK_Posts` PRIMARY KEY (`Id`),
+        CONSTRAINT `FK_Posts_Posts_ParentId` FOREIGN KEY (`ParentId`) REFERENCES `Posts` (`Id`) ON DELETE CASCADE,
+        CONSTRAINT `FK_Posts_Users_CreatorId` FOREIGN KEY (`CreatorId`) REFERENCES `Users` (`Id`)
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250901093419_Post') THEN
+
+    CREATE INDEX `IX_Posts_CreatorId` ON `Posts` (`CreatorId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250901093419_Post') THEN
+
+    CREATE INDEX `IX_Posts_ParentId` ON `Posts` (`ParentId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250901093419_Post') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20250901093419_Post', '9.0.14');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250904134132_UserFollowing') THEN
+
+    CREATE TABLE `UserFollowings` (
+        `FolloweeId` char(36) COLLATE ascii_general_ci NOT NULL,
+        `FollowerId` char(36) COLLATE ascii_general_ci NOT NULL,
+        `Followed` datetime(6) NOT NULL,
+        CONSTRAINT `PK_UserFollowings` PRIMARY KEY (`FollowerId`, `FolloweeId`),
+        CONSTRAINT `FK_UserFollowings_Users_FolloweeId` FOREIGN KEY (`FolloweeId`) REFERENCES `Users` (`Id`) ON DELETE CASCADE,
+        CONSTRAINT `FK_UserFollowings_Users_FollowerId` FOREIGN KEY (`FollowerId`) REFERENCES `Users` (`Id`) ON DELETE CASCADE
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250904134132_UserFollowing') THEN
+
+    CREATE INDEX `IX_UserFollowings_FolloweeId` ON `UserFollowings` (`FolloweeId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250904134132_UserFollowing') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20250904134132_UserFollowing', '9.0.14');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250910143414_UserDevice') THEN
+
+    ALTER TABLE `UserFollowings` ADD `IsSubscribed` tinyint(1) NOT NULL DEFAULT FALSE;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250910143414_UserDevice') THEN
+
+    CREATE TABLE `UserDevices` (
+        `Id` bigint NOT NULL AUTO_INCREMENT,
+        `Token` longtext CHARACTER SET utf8mb4 NOT NULL,
+        `Type` longtext CHARACTER SET utf8mb4 NOT NULL,
+        `UserId` char(36) COLLATE ascii_general_ci NOT NULL,
+        `Created` datetime(6) NOT NULL,
+        `Updated` datetime(6) NOT NULL,
+        CONSTRAINT `PK_UserDevices` PRIMARY KEY (`Id`),
+        CONSTRAINT `FK_UserDevices_Users_UserId` FOREIGN KEY (`UserId`) REFERENCES `Users` (`Id`)
+    ) CHARACTER SET=utf8mb4;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250910143414_UserDevice') THEN
+
+    CREATE INDEX `IX_UserDevices_UserId` ON `UserDevices` (`UserId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20250910143414_UserDevice') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20250910143414_UserDevice', '9.0.14');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20251014124326_UserBorn') THEN
+
+    ALTER TABLE `Users` ADD `Born` date NOT NULL DEFAULT '0001-01-01';
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20251014124326_UserBorn') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20251014124326_UserBorn', '9.0.14');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20251204101910_NormalizedNull') THEN
+
+    ALTER TABLE `Users` MODIFY COLUMN `UserNameNormalized` varchar(255) CHARACTER SET utf8mb4 NULL;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20251204101910_NormalizedNull') THEN
+
+    ALTER TABLE `Users` MODIFY COLUMN `EmailAddressNormalized` varchar(255) CHARACTER SET utf8mb4 NULL;
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20251204101910_NormalizedNull') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20251204101910_NormalizedNull', '9.0.14');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+COMMIT;
+
